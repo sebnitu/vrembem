7d827a8c2c8e6b978aa3b17f48e71d6c087eb4b9
Sebastian Nitu <nitu.seb@gmail.com>
2019-06-21 15:51:29 -0700

    Finalized the comments in utility and drawer components. Also added new API for switching between drawer and modal states

--- src/js/utility.js
+++ src/js/utility.js
@@ -10,7 +10,7 @@ export default class {
   /**
    * Get and output a breakpoint using it's key found in config.json
    * ---
-   * @param {String} - The key to search for in the breakpoints object
+   * @param {String} key - The key to search for in the breakpoints object
    * @returns {String} - The pixel value of the breakpoint as a string
    */
   static getBreakpoint(key) {
@@ -20,14 +20,12 @@ export default class {
   /**
    * Checks if an element has a class or not
    * ---
-   * @param {Object} || {Nodelist} - Element(s) to check class(es) on
-   * @param {String} || {Array} - Class(es) to check
+   * @param {Node} el - Element(s) to check class(es) on
+   * @param {String || Array} c - Class(es) to check
    * @returns {Boolean} - Returns true if class exists, otherwise false
    */
   static hasClass(el, c) {
-    if (!el.forEach) {
-      el = this.toArray(el)
-    }
+    el = (el.forEach) ? el : this.toArray(el)
     c = this.toArray(c)
     return c.some( function (c) {
       let has = false
@@ -43,13 +41,11 @@ export default class {
   /**
    * Adds a class or classes to an element
    * ---
-   * @param {Object} || {Nodelist} - Element(s) to add class(es) on
-   * @param {String} || {Array} - Class(es) to add
+   * @param {Node} el - Element(s) to add class(es) on
+   * @param {String || Array} c - Class(es) to add
    */
   static addClass(el, c) {
-    if (!el.forEach) {
-      el = this.toArray(el)
-    }
+    el = (el.forEach) ? el : this.toArray(el)
     c = this.toArray(c)
     el.forEach((el) => {
       c.forEach((c) => {
@@ -61,13 +57,11 @@ export default class {
   /**
    * Remove a class or classes from an element
    * ---
-   * @param {Object} || {Nodelist} - Element(s) to remove class(es) from
-   * @param {String} || {Array} - Class(es) to remove
+   * @param {Node} el - Element(s) to remove class(es) from
+   * @param {String || Array} c - Class(es) to remove
    */
   static removeClass(el, c) {
-    if (!el.forEach) {
-      el = this.toArray(el)
-    }
+    el = (el.forEach) ? el : this.toArray(el)
     c = this.toArray(c)
     el.forEach((el) => {
       c.forEach((c) => {
@@ -79,13 +73,11 @@ export default class {
   /**
    * Toggle a class or classes on an element
    * ---
-   * @param {Object} || {Nodelist} - Element(s) to toggle class(es) on
-   * @param {String} || {Array} - Class(es) to toggle
+   * @param {Node} el - Element(s) to toggle class(es) on
+   * @param {String || Array} c - Class(es) to toggle
    */
   static toggleClass(el, c) {
-    if (!el.forEach) {
-      el = this.toArray(el)
-    }
+    el = (el.forEach) ? el : this.toArray(el)
     c = this.toArray(c)
     el.forEach((el) => {
       c.forEach((c) => {
@@ -98,9 +90,9 @@ export default class {
    * Find the closest parent element based on class. This is different from the
    * native .closest() method in that it doesn't check the current element.
    * ---
-   * @param {Object} - Element to start search on
-   * @param {String} || {Array} - Class(es) to check for
-   * @return {Element} - Closest parent element
+   * @param {Node} el - Element to start search on
+   * @param {String || Array} c - Class(es) to check for
+   * @return {Node} - Closest parent element
    */
   static closest(el, c) {
     while ((el = el.parentElement) && !this.hasClass(el, c))
@@ -111,7 +103,7 @@ export default class {
    * Converts a string or object to an array. If an array is passed, it's
    * returned as is. Anything else is returned as an array.
    * ---
-   * @param {String} || {Object} - String or object to convert to an array
+   * @param {Object} item - String or object to convert to an array
    * @return {Array} - Return the converted array
    */
   static toArray(item) {
