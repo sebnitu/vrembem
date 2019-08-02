78c05c1e3950a2a9ac42fa488577243d17e4df34
Sebastian Nitu <nitu.seb@gmail.com>
2019-08-02 08:59:57 -0700

    Switched from node-sass to sass package

--- packages/core/dist/scripts.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _utility = _interopRequireDefault(require("src/utility"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-//# sourceMappingURL=scripts.js.map
