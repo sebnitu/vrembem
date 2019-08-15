d7b17b8360330565b511004b4957d0c802f760f1
Sebastian Nitu <nitu.seb@gmail.com>
2019-08-15 11:45:08 -0700

    Add the all component

---
+++ packages/all/index.js
@@ -0,0 +1,11 @@
+import dismissible from "@vrembem/dismissible/index"
+import drawer from "@vrembem/drawer/index"
+import modal from "@vrembem/modal/index"
+import toggle from "@vrembem/toggle/index"
+
+export {
+  dismissible,
+  drawer,
+  modal,
+  toggle
+}
---
+++ packages/all/rollup.config.js
@@ -0,0 +1,42 @@
+import babel from "rollup-plugin-babel"
+import minify from "rollup-plugin-babel-minify"
+import commonjs from "rollup-plugin-commonjs"
+import resolve from "rollup-plugin-node-resolve"
+import pkg from "./package.json"
+
+let name = pkg.name.replace("@", "").replace("/all", "")
+
+export default [{
+  input: pkg.module,
+  output: [{
+    file: pkg.browser,
+    format: "iife",
+    name: name
+  }, {
+    file: pkg.main,
+    format: "cjs",
+    name: name
+  }],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      configFile: "../../.babelrc"
+    })
+  ]
+}, {
+  input: pkg.module,
+  output: {
+    file: pkg.browser.replace(".js", ".min.js"),
+    format: "iife",
+    name: name
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      configFile: "../../.babelrc"
+    }),
+    minify()
+  ]
+}]
