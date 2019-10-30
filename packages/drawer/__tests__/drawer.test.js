516de4c73310e89a810bf4ff7a0fe284146215f6
Sebastian Nitu <nitu.seb@gmail.com>
2019-10-30 09:59:42 -0700

    Add custom data attr and state class tests

--- packages/drawer/__tests__/drawer.test.js
+++ packages/drawer/__tests__/drawer.test.js
@@ -8,11 +8,24 @@ const markup = `
   <div class="drawer__wrapper">
     <div class="drawer" data-drawer="drawer-default">
       <div class="drawer__item">
-        <button data-drawer-close data-drawer-focus>Close</button>
+        <button data-drawer-close>Close</button>
       </div>
     </div>
     <div class="drawer__main">
       <button data-drawer-toggle="drawer-default">Drawer Toggle</button>
+    </div>
+  </div>
+`
+
+const markupCustomAttr = `
+  <div class="drawer__wrapper">
+    <div class="drawer" data-d="drawer-default">
+      <div class="drawer__item">
+        <button data-c>Close</button>
+      </div>
+    </div>
+    <div class="drawer__main">
+      <button data-t="drawer-default">Drawer Toggle</button>
     </div>
   </div>
 `
@@ -49,5 +62,66 @@ test("should apply state classes on 'click' and 'transition end' events", () => 
   expect(el).not.toHaveClass("is-closing")
 })
 
-// TODO: Test that using custom state classes works
-// TODO: Test that using custom data attributes works
+test("should apply state classes with custom data attributes", () => {
+  document.body.innerHTML = markupCustomAttr
+  drawer = new Drawer({
+    autoInit: true,
+    dataDrawer: "d",
+    dataToggle: "t",
+    dataClose: "c"
+  })
+  const el = document.querySelector("[data-d]")
+  const btnOpen = document.querySelector("[data-t]")
+  const btnClose = document.querySelector("[data-c]")
+
+  expect(el).toHaveClass("drawer")
+
+  btnOpen.click()
+  expect(el).toHaveClass("is-opening")
+
+  el.dispatchEvent(ev)
+  expect(el).toHaveClass("is-open")
+
+  btnClose.click()
+  expect(el).toHaveClass("is-closing")
+
+  el.dispatchEvent(ev)
+  expect(el).toHaveClass("drawer")
+  expect(el).not.toHaveClass("is-opening")
+  expect(el).not.toHaveClass("is-open")
+  expect(el).not.toHaveClass("is-closing")
+})
+
+test("should apply custom state classes", () => {
+  document.body.innerHTML = markup
+  drawer = new Drawer({
+    autoInit: true,
+    stateOpen: "on",
+    stateOpening: "enable",
+    stateClosing: "disable",
+    stateClosed: "off"
+  })
+  const el = document.querySelector("[data-drawer]")
+  const btnOpen = document.querySelector("[data-drawer-toggle]")
+  const btnClose = document.querySelector("[data-drawer-close]")
+
+  expect(el).toHaveClass("drawer")
+
+  btnOpen.click()
+  expect(el).toHaveClass("enable")
+
+  el.dispatchEvent(ev)
+  expect(el).toHaveClass("on")
+
+  btnClose.click()
+  expect(el).toHaveClass("disable")
+
+  el.dispatchEvent(ev)
+  expect(el).toHaveClass("drawer")
+  expect(el).not.toHaveClass("on")
+  expect(el).not.toHaveClass("enable")
+  expect(el).not.toHaveClass("disable")
+
+  const state = JSON.parse(localStorage.getItem("DrawerState"))
+  expect(state["drawer-default"]).toMatch("off")
+})
