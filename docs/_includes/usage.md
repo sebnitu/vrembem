f9b8fdba10b2d24da6a85b3fb63bad220a01a33b
Sebastian Nitu <nitu.seb@gmail.com>
2019-11-06 12:11:28 -0800

    Update docs for core and base type spacing

--- docs/_includes/usage.md
+++ docs/_includes/usage.md
@@ -1,8 +1,10 @@
 <div class="type" markdown="1">
 
+{% comment %}
 <div class="flag">
   <span>Usage</span>
 </div>
+{% endcomment %}
 
 {% if page.usage.npm %}
 ```
@@ -10,13 +12,13 @@ npm install {{ page.usage.npm }}
 ```
 {% endif %}
 
-{% if page.usage.scss %}
+{% if page.usage.scss and page.title != "Core" %}
 ```scss
 @import "{{ page.usage.scss }}";
 ```
 {% endif %}
 
-{% if page.usage.js %}
+{% if page.usage.js and page.title != "Core" %}
 ```js
 import { {{ page.title }} } from "{{ page.usage.js }}"
 ```
--- docs/dist/scripts.cjs.js
+++ docs/dist/scripts.cjs.js
@@ -1,19 +1,5 @@
 'use strict';
 
-var breakpoint = {
-  xs: "480px",
-  sm: "620px",
-  md: "760px",
-  lg: "990px",
-  xl: "1380px"
-};
-
-var camelCase = function camelCase(str) {
-  return str.replace(/-([a-z])/g, function (g) {
-    return g[1].toUpperCase();
-  });
-};
-
 var addClass = function addClass(el) {
   for (var _len = arguments.length, cl = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
     cl[_key - 1] = arguments[_key];
@@ -24,6 +10,12 @@ var addClass = function addClass(el) {
     var _el$classList;
 
     (_el$classList = el.classList).add.apply(_el$classList, cl);
+  });
+};
+
+var camelCase = function camelCase(str) {
+  return str.replace(/-([a-z])/g, function (g) {
+    return g[1].toUpperCase();
   });
 };
 
@@ -53,6 +45,14 @@ var removeClass = function removeClass(el) {
 
     (_el$classList = el.classList).remove.apply(_el$classList, cl);
   });
+};
+
+var breakpoint = {
+  xs: "480px",
+  sm: "620px",
+  md: "760px",
+  lg: "990px",
+  xl: "1380px"
 };
 
 function _defineProperty(obj, key, value) {
