17a0f988cdf8ce13670c0531eaea28805234c896
sebnitu <nitu.seb@gmail.com>
2020-08-06 20:17:53 -0700

    test: add test coverage for focus trap functionality

--- packages/drawer/tests/focus.test.js
+++ packages/drawer/tests/focus.test.js
@@ -1,21 +1,34 @@
 import { Drawer } from '../index.js';
-import { transition } from './helpers/transition';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import { transition } from './helpers/transition';
 
 let drawer;
 
 const markup = `
   <div class="drawer__wrapper">
-    <div class="drawer is-closed" data-drawer="drawer-one" tabindex="-1">
+    <div class="drawer is-closed" data-drawer="drawer-one">
       <div data-drawer-dialog class="drawer__dialog">
         <button class="close-one" data-drawer-close>Close</button>
       </div>
     </div>
-    <div class="drawer is-closed" data-drawer="drawer-two" tabindex="-1">
+    <div class="drawer is-closed" data-drawer="drawer-two">
       <div data-drawer-dialog class="drawer__dialog">
         <button class="close-two" data-drawer-close data-drawer-focus>
           Close
         </button>
+      </div>
+    </div>
+    <div class="drawer drawer_modal" data-drawer="drawer-modal">
+      <div data-drawer-dialog class="drawer__dialog">
+        <button class="first">Close</button>
+        <button>...</button>
+        <button class="last">Modal Two</button>
+      </div>
+    </div>
+    <div class="drawer drawer_modal" data-drawer="drawer-empty">
+      <div data-drawer-dialog class="drawer__dialog">
+        ...
       </div>
     </div>
     <div class="drawer__main">
@@ -34,7 +47,7 @@ const markup = `
 
 const markupCustomAttr = `
   <div class="drawer__wrapper">
-    <div class="drawer is-closed" data-drawer="drawer-one" tabindex="-1">
+    <div class="drawer is-closed" data-drawer="drawer-one">
       <div class="drawer__dialog">
         <button class="close-one" data-drawer-close data-focus>Close</button>
       </div>
@@ -125,4 +138,58 @@ test('should re-focus the target if open triggers while drawer is already opened
   btn.click();
   await transition(el);
   expect(elFocus).toHaveFocus();
+});
+
+test('should retain focus on drawer if nothing inner is focusable', async () => {
+  document.body.innerHTML = markup;
+  drawer = new Drawer({ autoInit: true });
+  const el = document.querySelector('[data-drawer="drawer-empty"');
+  const dialog = el.querySelector('[data-drawer-dialog]');
+
+  drawer.open('drawer-empty');
+  await transition(el);
+
+  expect(el).toHaveClass('is-opened');
+  expect(dialog).toHaveFocus();
+
+  userEvent.tab();
+  expect(dialog).toHaveFocus();
+  userEvent.tab({ shift: true });
+  expect(dialog).toHaveFocus();
+});
+
+test('should properly setup a focus trap when drawer is open', async () => {
+  document.body.innerHTML = markup;
+  drawer = new Drawer({ autoInit: true });
+  const el = document.querySelector('[data-drawer="drawer-modal"');
+  const dialog = el.querySelector('[data-drawer-dialog]');
+
+  drawer.open('drawer-modal');
+  await transition(el);
+
+  expect(el).toHaveClass('is-opened');
+  expect(dialog).toHaveFocus();
+
+  userEvent.tab({ shift: true });
+  expect(drawer.memory.focusableLast).toHaveFocus();
+
+  userEvent.tab();
+  expect(drawer.memory.focusableFirst).toHaveFocus();
+
+  userEvent.tab({ shift: true });
+  expect(drawer.memory.focusableLast).toHaveFocus();
+
+  userEvent.tab({ shift: true });
+  userEvent.tab({ shift: true });
+  userEvent.tab({ shift: true });
+  expect(drawer.memory.focusableLast).toHaveFocus();
+
+  userEvent.tab();
+  userEvent.tab();
+  userEvent.tab();
+  expect(drawer.memory.focusableLast).toHaveFocus();
+
+  expect(drawer.memory.focusable.length).toBe(3);
+  expect(drawer.memory.focusableFirst).toHaveClass('first');
+  expect(drawer.memory.focusableLast).toHaveClass('last');
 });
