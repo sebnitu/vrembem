3528e90539fa080f77a0275460f72e599b791864
sebnitu <nitu.seb@gmail.com>
2020-08-12 14:10:52 -0700

    docs: clean up bundles

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -2,34 +2,18 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
 
+const entry = './src/js/app.js';
 const name = 'vrembem';
+const babelConfig = {
+  babelHelpers: 'runtime',
+  rootMode: 'upward'
+};
 
 export default [{
-  input: pkg.module,
-  output: [{
-    file: pkg.browser,
-    format: 'iife',
-    name: name,
-    extend: true
-  }, {
-    file: pkg.main,
-    format: 'cjs',
-    name: name
-  }],
-  plugins: [
-    resolve(),
-    commonjs(),
-    babel({
-      babelHelpers: 'runtime',
-      rootMode: 'upward'
-    })
-  ]
-}, {
-  input: pkg.module,
+  input: entry,
   output: {
-    file: pkg.browser.replace('.js', '.min.js'),
+    file: './dist/scripts.js',
     format: 'iife',
     name: name,
     extend: true
@@ -37,10 +21,20 @@ export default [{
   plugins: [
     resolve(),
     commonjs(),
-    babel({
-      babelHelpers: 'runtime',
-      rootMode: 'upward'
-    }),
+    babel(babelConfig)
+  ]
+}, {
+  input: entry,
+  output: {
+    file: './dist/scripts.min.js',
+    format: 'iife',
+    name: name,
+    extend: true
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel(babelConfig),
     terser()
   ]
 }];
