d23632dbf3d430a05ba48c2059a655a81307f83d
sebnitu <nitu.seb@gmail.com>
2020-09-13 23:53:35 -0700

    chore: initial setup of microbundle for modal component

---
+++ packages/modal/dist/scripts.modern.js
@@ -0,0 +1,198 @@
+import { setInert, setOverflowHidden, focusTrigger, removeClass, addClass, FocusTrap, setTabindex, moveElement, hasClass, openTransition, focusTarget, closeTransition } from '@vrembem/core/index';
+
+var defaults = {
+  autoInit: false,
+  // Data attributes
+  dataModal: 'modal',
+  dataDialog: 'modal-dialog',
+  dataOpen: 'modal-open',
+  dataClose: 'modal-close',
+  dataFocus: 'modal-focus',
+  dataRequired: 'modal-required',
+  // State classes
+  stateOpened: 'is-opened',
+  stateOpening: 'is-opening',
+  stateClosing: 'is-closing',
+  stateClosed: 'is-closed',
+  // Selector
+  selectorInert: null,
+  selectorOverflow: 'body',
+  // Feature toggles
+  customEventPrefix: 'modal:',
+  moveModals: {
+    ref: null,
+    type: null
+  },
+  setTabindex: true,
+  transition: true
+};
+
+async function handlerClick(event) {
+  // Working catch
+  if (this.working) return; // Trigger click
+
+  const trigger = event.target.closest(`[data-${this.settings.dataOpen}]`);
+
+  if (trigger) {
+    event.preventDefault();
+    const modalKey = trigger.getAttribute(`data-${this.settings.dataOpen}`);
+    const fromModal = event.target.closest(`[data-${this.settings.dataModal}]`);
+    if (!fromModal) this.memory.trigger = trigger;
+    await this.close(!fromModal);
+    this.open(modalKey);
+    return;
+  } // Close click
+
+
+  if (event.target.closest(`[data-${this.settings.dataClose}]`)) {
+    event.preventDefault();
+    this.close();
+    return;
+  } // Root click
+
+
+  if (event.target.hasAttribute(`data-${this.settings.dataModal}`) && !event.target.hasAttribute(`data-${this.settings.dataRequired}`)) {
+    this.close();
+    return;
+  }
+}
+function handlerKeyup(event) {
+  // Working catch
+  if (this.working) return;
+
+  if (event.key === 'Escape' || event.keyCode === 27) {
+    const target = document.querySelector(`[data-${this.settings.dataModal}].${this.settings.stateOpened}`);
+
+    if (target && !target.hasAttribute(`data-${this.settings.dataRequired}`)) {
+      this.close();
+    }
+  }
+}
+
+function setInitialState(obj) {
+  const modals = document.querySelectorAll(`[data-${obj.settings.dataModal}]`);
+  modals.forEach(el => {
+    if (el.classList.contains(obj.settings.stateOpened)) {
+      setInert(false, obj.settings.selectorInert);
+      setOverflowHidden(false, obj.settings.selectorOverflow);
+      focusTrigger(obj);
+      obj.focusTrap.destroy();
+    }
+
+    removeClass(el, obj.settings.stateOpened, obj.settings.stateOpening, obj.settings.stateClosing);
+    addClass(el, obj.settings.stateClosed);
+  });
+}
+
+class Modal {
+  constructor(options) {
+    this.defaults = defaults;
+    this.settings = { ...this.defaults,
+      ...options
+    };
+    this.working = false;
+    this.memory = {};
+    this.focusTrap = new FocusTrap();
+    this.__handlerClick = handlerClick.bind(this);
+    this.__handlerKeyup = handlerKeyup.bind(this);
+    if (this.settings.autoInit) this.init();
+  }
+
+  init(options = null) {
+    if (options) this.settings = { ...this.settings,
+      ...options
+    };
+    this.moveModals();
+    this.setTabindex(this.settings.setTabindex);
+    this.setInitialState();
+    document.addEventListener('click', this.__handlerClick, false);
+    document.addEventListener('touchend', this.__handlerClick, false);
+    document.addEventListener('keyup', this.__handlerKeyup, false);
+  }
+
+  destroy() {
+    this.memory = {};
+    document.removeEventListener('click', this.__handlerClick, false);
+    document.removeEventListener('touchend', this.__handlerClick, false);
+    document.removeEventListener('keyup', this.__handlerKeyup, false);
+  }
+  /**
+   * Helpers
+   */
+
+
+  getModal(modalKey) {
+    if (typeof modalKey !== 'string') return modalKey;
+    return document.querySelector(`[data-${this.settings.dataModal}="${modalKey}"]`);
+  }
+
+  modalNotFound(key) {
+    return Promise.reject(new Error(`Did not find modal with key: "${key}"`));
+  }
+
+  setTabindex(state = true) {
+    const selectorTabindex = `
+      [data-${this.settings.dataModal}]
+      [data-${this.settings.dataDialog}]
+    `;
+    setTabindex(state, selectorTabindex);
+  }
+
+  setInitialState() {
+    setInitialState(this);
+  }
+
+  moveModals(type = this.settings.moveModals.type, ref = this.settings.moveModals.ref) {
+    const modals = document.querySelectorAll(`[data-${this.settings.dataModal}]`);
+    if (modals.length) moveElement(modals, type, ref);
+  }
+  /**
+   * Change state functionality
+   */
+
+
+  async open(modalKey) {
+    const modal = this.getModal(modalKey);
+    if (!modal) return this.modalNotFound(modalKey);
+
+    if (hasClass(modal, this.settings.stateClosed)) {
+      this.working = true;
+      setOverflowHidden(true, this.settings.selectorOverflow);
+      await openTransition(modal, this.settings);
+      this.focusTrap.init(modal);
+      focusTarget(modal, this.settings);
+      setInert(true, this.settings.selectorInert);
+      modal.dispatchEvent(new CustomEvent(this.settings.customEventPrefix + 'opened', {
+        bubbles: true
+      }));
+      this.working = false;
+      return modal;
+    } else {
+      return modal;
+    }
+  }
+
+  async close(returnFocus = true) {
+    const modal = document.querySelector(`[data-${this.settings.dataModal}].${this.settings.stateOpened}`);
+
+    if (modal) {
+      this.working = true;
+      setInert(false, this.settings.selectorInert);
+      setOverflowHidden(false, this.settings.selectorOverflow);
+      await closeTransition(modal, this.settings);
+      if (returnFocus) focusTrigger(this);
+      this.focusTrap.destroy();
+      modal.dispatchEvent(new CustomEvent(this.settings.customEventPrefix + 'closed', {
+        bubbles: true
+      }));
+      this.working = false;
+      return modal;
+    } else {
+      return modal;
+    }
+  }
+
+}
+
+export default Modal;
+//# sourceMappingURL=scripts.modern.js.map
---
+++ packages/modal/dist/scripts.umd.js
@@ -0,0 +1,275 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@vrembem/core/index')) :
+  typeof define === 'function' && define.amd ? define(['@vrembem/core/index'], factory) :
+  (global = global || self, (global.vrembem = global.vrembem || {}, global.vrembem.Modal = factory(global.index)));
+}(this, (function (index) {
+  function _extends() {
+    _extends = Object.assign || function (target) {
+      for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];
+
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+
+      return target;
+    };
+
+    return _extends.apply(this, arguments);
+  }
+
+  var defaults = {
+    autoInit: false,
+    // Data attributes
+    dataModal: 'modal',
+    dataDialog: 'modal-dialog',
+    dataOpen: 'modal-open',
+    dataClose: 'modal-close',
+    dataFocus: 'modal-focus',
+    dataRequired: 'modal-required',
+    // State classes
+    stateOpened: 'is-opened',
+    stateOpening: 'is-opening',
+    stateClosing: 'is-closing',
+    stateClosed: 'is-closed',
+    // Selector
+    selectorInert: null,
+    selectorOverflow: 'body',
+    // Feature toggles
+    customEventPrefix: 'modal:',
+    moveModals: {
+      ref: null,
+      type: null
+    },
+    setTabindex: true,
+    transition: true
+  };
+
+  var handlerClick = function handlerClick(event) {
+    try {
+      var _exit2;
+
+      var _this2 = this;
+
+      function _temp3(_result) {
+        if (_exit2) return _result;
+
+        // Close click
+        if (event.target.closest("[data-" + _this2.settings.dataClose + "]")) {
+          event.preventDefault();
+
+          _this2.close();
+
+          return;
+        } // Root click
+
+
+        if (event.target.hasAttribute("data-" + _this2.settings.dataModal) && !event.target.hasAttribute("data-" + _this2.settings.dataRequired)) {
+          _this2.close();
+        }
+      }
+
+      // Working catch
+      if (_this2.working) return Promise.resolve(); // Trigger click
+
+      var trigger = event.target.closest("[data-" + _this2.settings.dataOpen + "]");
+
+      var _temp4 = function () {
+        if (trigger) {
+          event.preventDefault();
+          var modalKey = trigger.getAttribute("data-" + _this2.settings.dataOpen);
+          var fromModal = event.target.closest("[data-" + _this2.settings.dataModal + "]");
+          if (!fromModal) _this2.memory.trigger = trigger;
+          return Promise.resolve(_this2.close(!fromModal)).then(function () {
+            _this2.open(modalKey);
+
+            _exit2 = 1;
+          });
+        }
+      }();
+
+      return Promise.resolve(_temp4 && _temp4.then ? _temp4.then(_temp3) : _temp3(_temp4));
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  function handlerKeyup(event) {
+    // Working catch
+    if (this.working) return;
+
+    if (event.key === 'Escape' || event.keyCode === 27) {
+      var target = document.querySelector("[data-" + this.settings.dataModal + "]." + this.settings.stateOpened);
+
+      if (target && !target.hasAttribute("data-" + this.settings.dataRequired)) {
+        this.close();
+      }
+    }
+  }
+
+  function setInitialState(obj) {
+    var modals = document.querySelectorAll("[data-" + obj.settings.dataModal + "]");
+    modals.forEach(function (el) {
+      if (el.classList.contains(obj.settings.stateOpened)) {
+        index.setInert(false, obj.settings.selectorInert);
+        index.setOverflowHidden(false, obj.settings.selectorOverflow);
+        index.focusTrigger(obj);
+        obj.focusTrap.destroy();
+      }
+
+      index.removeClass(el, obj.settings.stateOpened, obj.settings.stateOpening, obj.settings.stateClosing);
+      index.addClass(el, obj.settings.stateClosed);
+    });
+  }
+
+  var Modal = /*#__PURE__*/function () {
+    function Modal(options) {
+      this.defaults = defaults;
+      this.settings = _extends({}, this.defaults, options);
+      this.working = false;
+      this.memory = {};
+      this.focusTrap = new index.FocusTrap();
+      this.__handlerClick = handlerClick.bind(this);
+      this.__handlerKeyup = handlerKeyup.bind(this);
+      if (this.settings.autoInit) this.init();
+    }
+
+    var _proto = Modal.prototype;
+
+    _proto.init = function init(options) {
+      if (options === void 0) {
+        options = null;
+      }
+
+      if (options) this.settings = _extends({}, this.settings, options);
+      this.moveModals();
+      this.setTabindex(this.settings.setTabindex);
+      this.setInitialState();
+      document.addEventListener('click', this.__handlerClick, false);
+      document.addEventListener('touchend', this.__handlerClick, false);
+      document.addEventListener('keyup', this.__handlerKeyup, false);
+    };
+
+    _proto.destroy = function destroy() {
+      this.memory = {};
+      document.removeEventListener('click', this.__handlerClick, false);
+      document.removeEventListener('touchend', this.__handlerClick, false);
+      document.removeEventListener('keyup', this.__handlerKeyup, false);
+    }
+    /**
+     * Helpers
+     */
+    ;
+
+    _proto.getModal = function getModal(modalKey) {
+      if (typeof modalKey !== 'string') return modalKey;
+      return document.querySelector("[data-" + this.settings.dataModal + "=\"" + modalKey + "\"]");
+    };
+
+    _proto.modalNotFound = function modalNotFound(key) {
+      return Promise.reject(new Error("Did not find modal with key: \"" + key + "\""));
+    };
+
+    _proto.setTabindex = function setTabindex(state) {
+      if (state === void 0) {
+        state = true;
+      }
+
+      var selectorTabindex = "\n      [data-" + this.settings.dataModal + "]\n      [data-" + this.settings.dataDialog + "]\n    ";
+
+      index.setTabindex(state, selectorTabindex);
+    };
+
+    _proto.setInitialState = function setInitialState$1() {
+      setInitialState(this);
+    };
+
+    _proto.moveModals = function moveModals(type, ref) {
+      if (type === void 0) {
+        type = this.settings.moveModals.type;
+      }
+
+      if (ref === void 0) {
+        ref = this.settings.moveModals.ref;
+      }
+
+      var modals = document.querySelectorAll("[data-" + this.settings.dataModal + "]");
+      if (modals.length) index.moveElement(modals, type, ref);
+    }
+    /**
+     * Change state functionality
+     */
+    ;
+
+    _proto.open = function open(modalKey) {
+      try {
+        var _this2 = this;
+
+        var modal = _this2.getModal(modalKey);
+
+        if (!modal) return Promise.resolve(_this2.modalNotFound(modalKey));
+
+        if (index.hasClass(modal, _this2.settings.stateClosed)) {
+          _this2.working = true;
+          index.setOverflowHidden(true, _this2.settings.selectorOverflow);
+          return Promise.resolve(index.openTransition(modal, _this2.settings)).then(function () {
+            _this2.focusTrap.init(modal);
+
+            index.focusTarget(modal, _this2.settings);
+            index.setInert(true, _this2.settings.selectorInert);
+            modal.dispatchEvent(new CustomEvent(_this2.settings.customEventPrefix + 'opened', {
+              bubbles: true
+            }));
+            _this2.working = false;
+            return modal;
+          });
+        } else {
+          return Promise.resolve(modal);
+        }
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    };
+
+    _proto.close = function close(returnFocus) {
+      if (returnFocus === void 0) {
+        returnFocus = true;
+      }
+
+      try {
+        var _this4 = this;
+
+        var modal = document.querySelector("[data-" + _this4.settings.dataModal + "]." + _this4.settings.stateOpened);
+
+        if (modal) {
+          _this4.working = true;
+          index.setInert(false, _this4.settings.selectorInert);
+          index.setOverflowHidden(false, _this4.settings.selectorOverflow);
+          return Promise.resolve(index.closeTransition(modal, _this4.settings)).then(function () {
+            if (returnFocus) index.focusTrigger(_this4);
+
+            _this4.focusTrap.destroy();
+
+            modal.dispatchEvent(new CustomEvent(_this4.settings.customEventPrefix + 'closed', {
+              bubbles: true
+            }));
+            _this4.working = false;
+            return modal;
+          });
+        } else {
+          return Promise.resolve(modal);
+        }
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    };
+
+    return Modal;
+  }();
+
+  return Modal;
+
+})));
+//# sourceMappingURL=scripts.umd.js.map
