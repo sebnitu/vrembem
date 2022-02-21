0c2fbaf66bfeaff0e85937170e9c793bac8b8400
Sebastian Nitu <nitu.seb@gmail.com>
2022-02-20 17:38:50 -0800

    Apply JSDoc style comments for JS modules

--- packages/core/src/js/removeClass.js
+++ packages/core/src/js/removeClass.js
@@ -1,10 +1,8 @@
 /**
- * Remove a class or classes from an element or NodeList
- * ---
- * @param {Node || NodeList} el - Element(s) to remove class(es) from
- * @param {String || Array} cl - Class(es) to remove
+ * Remove a class or classes from an element or NodeList.
+ * @param {Node || NodeList} el - Element(s) to remove class(es) from.
+ * @param {String || Array} cl - Class(es) to remove.
  */
-
 export const removeClass = (el, ...cl) => {
   el = (el.forEach) ? el : [el];
   el.forEach((el) => {
