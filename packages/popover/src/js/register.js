2deaf65c651a715392da93d784e12ebd4b1e4e4b
sebnitu <nitu.seb@gmail.com>
2022-02-26 15:34:22 -0800

    Simplify the popover register function

--- packages/popover/src/js/register.js
+++ packages/popover/src/js/register.js
@@ -6,14 +6,13 @@ import { close, closeCheck } from './close';
 import { open } from './open';
 
 export function register(trigger, target) {
-  // Deregister popover if it already exists in the collection
+  // Deregister item if it already exists in the collection
   this.deregister(target.id);
 
-  // Create popper instance
-  const popperInstance = createPopper(trigger, target);
-
-  // Save root this for use inside object & create methods API
+  // Save root this for use inside methods API
   const root = this;
+
+  // Setup methods API
   const methods = {
     open() {
       open.call(root, this);
@@ -26,33 +25,33 @@ export function register(trigger, target) {
     }
   };
 
-  // Build popover object and push to collection array
-  const popover = {
+  // Build object
+  const item = {
     id: target.id,
     state: 'closed',
     trigger: trigger,
     target: target,
-    popper: popperInstance,
+    popper: createPopper(trigger, target),
     config: getConfig(target, this.settings),
     ...methods
   };
 
   // Setup event listeners
-  registerEventListeners.call(this, popover);
+  registerEventListeners.call(this, item);
 
-  // Set initial state of popover
-  if (popover.target.classList.contains(this.settings.stateActive)) {
-    popover.open();
-    documentClick.call(this, popover);
+  // Set initial state
+  if (item.target.classList.contains(this.settings.stateActive)) {
+    item.open();
+    documentClick.call(this, item);
   } else {
-    popover.close();
+    item.close();
   }
 
   // Add item to collection
-  this.collection.push(popover);
+  this.collection.push(item);
 
-  // Return the popover object
-  return popover;
+  // Return the registered object
+  return item;
 }
 
 export function deregister(popover) {
