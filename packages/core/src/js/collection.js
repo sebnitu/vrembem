8fd658ef5b953e8676532b900d7372b25c632a61
sebnitu <nitu.seb@gmail.com>
2022-02-27 23:38:20 -0800

    Add promises to collections module

--- packages/core/src/js/collection.js
+++ packages/core/src/js/collection.js
@@ -23,16 +23,16 @@ export class Collection {
     return this.collection;
   }
 
-  registerCollection(items) {
-    items.forEach((item) => {
-      this.register(item);
+  async registerCollection(items) {
+    await items.forEach(async (item) => {
+      await this.register(item);
     });
     return this.collection;
   }
 
-  deregisterCollection() {
+  async deregisterCollection() {
     while (this.collection.length > 0) {
-      this.deregister(this.collection[0]);
+      await this.deregister(this.collection[0]);
     }
     return this.collection;
   }
