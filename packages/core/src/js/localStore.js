08456a8c7b421297648f76c1dd15ab8c581aed6f
sebnitu <nitu.seb@gmail.com>
2022-03-25 22:37:56 -0700

    Add test coverage for localStore module

--- packages/core/src/js/localStore.js
+++ packages/core/src/js/localStore.js
@@ -19,6 +19,7 @@ export function localStore(key, enable = true) {
     deleteProperty: (target, property) => {
       delete target[property];
       if (enable) setStore(target);
+      return true;
     }
   });
 }
