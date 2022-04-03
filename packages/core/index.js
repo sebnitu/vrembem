b7f1578210c86350034c5a099b177dc1981e2060
sebnitu <nitu.seb@gmail.com>
2022-04-02 21:16:15 -0700

    Remove unused core helper modules

--- packages/core/index.js
+++ packages/core/index.js
@@ -2,16 +2,10 @@ export * from './src/js/Breakpoint';
 export * from './src/js/Collection';
 export * from './src/js/FocusTrap';
 
-export * from './src/js/addClass';
-export * from './src/js/camelCase';
 export * from './src/js/getConfig';
-export * from './src/js/hasClass';
-export * from './src/js/hyphenCase';
 export * from './src/js/localStore';
-export * from './src/js/removeClass';
 export * from './src/js/setInert';
 export * from './src/js/setOverflowHidden';
 export * from './src/js/teleport';
-export * from './src/js/toggleClass';
 export * from './src/js/transition';
 export * from './src/js/updateGlobalState';
