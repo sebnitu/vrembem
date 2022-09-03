f03c1d39c917b79aaee0195251a77ffc5219abcd
sebnitu <nitu.seb@gmail.com>
2022-09-02 22:09:26 -0700

    Use vrembem in astro docs

--- astro/src/components/Counter.vue
+++ astro/src/components/Counter.vue
@@ -5,5 +5,5 @@ const count = ref(0);
 </script>
 
 <template>
-  <button @click="count++">Count: {{ count }}</button>
+  <button class="button" @click="count++">Count: {{ count }}</button>
 </template>
