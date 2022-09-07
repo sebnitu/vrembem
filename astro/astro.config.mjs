e096d6aafff39dc9f29dcd7f99c71210a63f9ad4
sebnitu <nitu.seb@gmail.com>
2022-09-07 09:59:08 -0700

    Add addClasses rehype plugin

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
+
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
+import addClasses from 'rehype-add-classes';
+
+const additions = {
+  table: 'table table_style_bordered table_zebra table_hover'
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), mdx()]
+  integrations: [vue(), mdx({
+    rehypePlugins: [[addClasses, additions]],
+  })]
 });
