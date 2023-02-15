f56bfc55beae482b0ccdd2966007ae33a1665309
sebnitu <nitu.seb@gmail.com>
2023-02-15 10:09:37 -0800

    Convert packages md files into new collections api

---
+++ astro/src/content/config.ts
@@ -0,0 +1,14 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a schema for each collection you'd like to validate.
+const packages = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    package: z.string(),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = { packages };
