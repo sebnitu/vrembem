10345f8958b170e0cb1aca1cb61109cfde402f51
sebnitu <nitu.seb@gmail.com>
2023-07-22 10:21:19 -0700

    Add guard clause to prevent transition event bubbling

--- packages/core/src/js/transition.js
+++ packages/core/src/js/transition.js
@@ -1,15 +1,26 @@
 export const openTransition = (el, settings) => {
   return new Promise((resolve) => {
+    // Check if transitions are enabled.
     if (settings.transition) {
+      // Toggle classes for opening transition.
       el.classList.remove(settings.stateClosed);
       el.classList.add(settings.stateOpening);
-      el.addEventListener('transitionend', function _f() {
+
+      // Add event listener for when the transition is finished.
+      el.addEventListener('transitionend', function _f(event) {
+        // Prevent child transition bubbling from firing this event.
+        if (event.target != el) return;
+
+        // Toggle final opened state classes.
         el.classList.add(settings.stateOpened);
         el.classList.remove(settings.stateOpening);
+
+        // Resolve the promise and remove the event listener.
         resolve(el);
         this.removeEventListener('transitionend', _f);
       });
     } else {
+      // Toggle final opened state classes and resolve the promise.
       el.classList.add(settings.stateOpened);
       el.classList.remove(settings.stateClosed);
       resolve(el);
@@ -19,16 +30,27 @@ export const openTransition = (el, settings) => {
 
 export const closeTransition = (el, settings) => {
   return new Promise((resolve) => {
+    // Check if transitions are enabled.
     if (settings.transition) {
+      // Toggle classes for closing transition.
       el.classList.add(settings.stateClosing);
       el.classList.remove(settings.stateOpened);
-      el.addEventListener('transitionend', function _f() {
+
+      // Add event listener for when the transition is finished.
+      el.addEventListener('transitionend', function _f(event) {
+        // Prevent child transition bubbling from firing this event.
+        if (event.target != el) return;
+        
+        // Toggle final closed state classes.
         el.classList.remove(settings.stateClosing);
         el.classList.add(settings.stateClosed);
+
+        // Resolve the promise and remove the event listener.
         resolve(el);
         this.removeEventListener('transitionend', _f);
       });
     } else {
+      // Toggle final closed state classes and resolve the promise.
       el.classList.add(settings.stateClosed);
       el.classList.remove(settings.stateOpened);
       resolve(el);
