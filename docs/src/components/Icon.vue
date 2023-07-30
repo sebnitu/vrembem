2323f329e0fce47a388f7b27a0e2a594fbeabae0
sebnitu <nitu.seb@gmail.com>
2023-07-30 00:42:26 -0700

    Add feather-icon component and adjust icon styles

---
+++ docs/src/components/Icon.vue
@@ -0,0 +1,37 @@
+<template>
+  <span :class="class" v-html="svg"></span>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import feather from 'feather-icons';
+
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'feather'
+    },
+    class: {
+      type: String,
+      default: 'display-flex'
+    },
+    classes: {
+      type: String,
+      default: ''
+    }
+  },
+  setup(props) {
+    const svg = ref(null);
+    const classes = `icon${(props.classes) ? ' ' + props.classes : ''}`;
+
+    svg.value = feather.icons[props.name].toSvg({
+      class: classes
+    });
+
+    return {
+      svg
+    }
+  }
+}
+</script>
