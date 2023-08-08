72ff075256e9c64dd97f8207c2a454e01da93de2
sebnitu <nitu.seb@gmail.com>
2023-08-07 20:13:50 -0700

    refactor: rename drawer var to entry internally

--- packages/drawer/src/js/open.js
+++ packages/drawer/src/js/open.js
@@ -3,40 +3,40 @@ import { updateFocusState, getDrawer } from './helpers';
 
 export async function open(query, transition, focus = true) {
   // Get the drawer from collection.
-  const drawer = getDrawer.call(this, query);
+  const entry = getDrawer.call(this, query);
 
   // Get the modal configuration.
-  const config = { ...this.settings, ...drawer.settings };
+  const config = { ...this.settings, ...entry.settings };
 
   // Add transition parameter to configuration.
   if (transition !== undefined) config.transition = transition;
 
   // If drawer is not opened.
-  if (drawer.state != 'opened') {
+  if (entry.state != 'opened') {
     // Update drawer state.
-    drawer.state = 'opening';
+    entry.state = 'opening';
 
     // Run the open transition.
-    await openTransition(drawer.el, config);
+    await openTransition(entry.el, config);
 
     // Update the global state if mode is modal.
-    if (drawer.mode === 'modal') updateGlobalState(true, config);
+    if (entry.mode === 'modal') updateGlobalState(true, config);
 
     // Update drawer state.
-    drawer.state = 'opened';
+    entry.state = 'opened';
   }
 
   // Set focus to the drawer element if the focus param is true.
   if (focus) {
-    updateFocusState.call(this, drawer);
+    updateFocusState.call(this, entry);
   }
 
   // Dispatch custom opened event.
-  drawer.el.dispatchEvent(new CustomEvent(config.customEventPrefix + 'opened', {
+  entry.el.dispatchEvent(new CustomEvent(config.customEventPrefix + 'opened', {
     detail: this,
     bubbles: true
   }));
 
   // Return the drawer.
-  return drawer;
+  return entry;
 }
