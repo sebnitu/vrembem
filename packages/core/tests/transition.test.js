d987eccb081d330b8a315cd32b5ef84e5eef4234
sebnitu <nitu.seb@gmail.com>
2023-08-10 10:17:37 -0700

    test: update transition tests

--- packages/core/tests/transition.test.js
+++ packages/core/tests/transition.test.js
@@ -1,4 +1,4 @@
-import { openTransition, closeTransition } from '../index';
+import { transition } from '../index';
 import '@testing-library/jest-dom/extend-expect';
 
 document.body.innerHTML = `
@@ -7,94 +7,37 @@ document.body.innerHTML = `
   </div>
 `;
 
+vi.useFakeTimers();
+
 const el = document.querySelector('div');
 const btn = document.querySelector('button');
-const classes = {
-  stateOpened: 'is-opened',
-  stateOpening: 'is-opening',
-  stateClosing: 'is-closing',
-  stateClosed: 'is-closed',
-  transition: true
+const open = {
+  start: 'is-opening',
+  finish: 'is-opened'
+};
+const close = {
+  start: 'is-closing',
+  finish: 'is-closed'
 };
 
-test('should go through opening transition classes when openTransition is called', () => {
-  openTransition(el, classes);
+test('should go through opening transition classes when transition is called', () => {
+  transition(el, close, open);
   expect(el).toHaveClass('is-opening');
   expect(el.classList.length).toBe(2);
 
-  el.dispatchEvent(new Event('transitionend'));
+  vi.runAllTimers();
+
   expect(el).toHaveClass('is-opened');
   expect(el.classList.length).toBe(2);
 });
 
-test('should go through closing transition classes when closeTransition is called', () => {
-  closeTransition(el, classes);
+test('should go through closing transition classes when transition is called', () => {
+  transition(el, open, close);
   expect(el).toHaveClass('is-closing');
   expect(el.classList.length).toBe(2);
 
-  el.dispatchEvent(new Event('transitionend'));
-  expect(el).toHaveClass('is-closed');
-  expect(el.classList.length).toBe(2);
-});
+  vi.runAllTimers();
 
-test('should open immediatly if openTransition is called with transitions disabled', () => {
-  openTransition(el, { ...classes, ...{ transition: false } });
-  expect(el).toHaveClass('is-opened');
-  expect(el.classList.length).toBe(2);
-});
-
-test('should close immediatly if closeTransition is called with transitions disabled', () => {
-  closeTransition(el, { ...classes, ...{ transition: false } });
-  expect(el).toHaveClass('is-closed');
-  expect(el.classList.length).toBe(2);
-});
-
-test('should return a promise when openTransition is called', () => {
-  const item = openTransition(el, classes);
-  expect(el).toHaveClass('is-opening');
-  expect(el.classList.length).toBe(2);
-  el.dispatchEvent(new Event('transitionend'));
-  item.then(() => {
-    expect(el).toHaveClass('is-opened');
-    expect(el.classList.length).toBe(2);
-  });
-});
-
-test('should return a promise when closeTransition is called', () => {
-  const item = closeTransition(el, classes);
-  expect(el).toHaveClass('is-closing');
-  expect(el.classList.length).toBe(2);
-  el.dispatchEvent(new Event('transitionend'));
-  item.then(() => {
-    expect(el).toHaveClass('is-closed');
-    expect(el.classList.length).toBe(2);
-  });
-});
-
-test('should not run opening transition if the transitionend event fired from a child element', () => {
-  openTransition(el, classes);
-  expect(el).toHaveClass('is-opening');
-  expect(el.classList.length).toBe(2);
-
-  btn.dispatchEvent(new Event('transitionend', { bubbles: true }));
-  expect(el).toHaveClass('is-opening');
-  expect(el.classList.length).toBe(2);
-
-  el.dispatchEvent(new Event('transitionend'));
-  expect(el).toHaveClass('is-opened');
-  expect(el.classList.length).toBe(2);
-});
-
-test('should not closing transition if the transitionend event fired from a child element', () => {
-  closeTransition(el, classes);
-  expect(el).toHaveClass('is-closing');
-  expect(el.classList.length).toBe(2);
-
-  btn.dispatchEvent(new Event('transitionend', { bubbles: true }));
-  expect(el).toHaveClass('is-closing');
-  expect(el.classList.length).toBe(2);
-
-  el.dispatchEvent(new Event('transitionend'));
   expect(el).toHaveClass('is-closed');
   expect(el.classList.length).toBe(2);
 });
