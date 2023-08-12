a003cc7f93ac4facfcf3bebbba6d39aa3484663c
sebnitu <nitu.seb@gmail.com>
2023-08-12 09:17:08 -0700

    chore: fix formatting

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-import vue from "@astrojs/vue";
-import mdx from "@astrojs/mdx";
+import vue from '@astrojs/vue';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   integrations: [vue(), mdx()],
