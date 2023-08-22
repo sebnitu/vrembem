0acaf2e25c46aabf21c467ec9194f0e509c84b54
Sebastian Nitu <nitu.seb@gmail.com>
2023-08-22 11:48:35 -0700

    Add header anchors to page content (#1412)
    This PR sets up a new header anchor module for appending `#` anchors to content headings.

---
+++ docs/src/modules/headerAnchor.js
@@ -0,0 +1,30 @@
+const headerAnchor = (options = {}) => {
+  const settings = {
+    prefix: ".layout__content",
+    headings: ["h1", "h2", "h3", "h4", "h5", "h6"],
+    ...options
+  };
+
+  function mount() {
+    settings.headings.forEach((heading) => {
+      const els = document.querySelectorAll(`${settings.prefix} ${heading}`);
+      els.forEach((el) => {
+        const elText = el.innerHTML;
+        const elId = el.getAttribute("id");
+        const anchor = document.createElement("a");
+        anchor.setAttribute("class", "header-anchor");
+        anchor.setAttribute("href", "#" + elId);
+        anchor.setAttribute("aria-label", `Permalink to "${elText}"`);
+        anchor.innerHTML = "#";
+        el.append(anchor);
+      });
+    });
+  }
+
+  return {
+    settings,
+    mount
+  };
+};
+
+export { headerAnchor };
