bf5b22b4dadaf983b0babe8d430db71a8a0c8583
sebnitu <nitu.seb@gmail.com>
2023-08-30 12:28:42 -0700

    Split the theme switcher into additional components for multiple rendering

---
+++ docs/src/components/ThemeSwitcherMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="menu">
+    <li class="menu__item">
+      <button @click="changeTheme('default')" class="menu__action" :class="store.theme === 'default' && 'is-active'">
+        <Icon name="box" />
+        <span>OS Default</span>
+      </button>
+    </li>
+    <li class="menu__item">
+      <button @click="changeTheme('light')" class="menu__action" :class="store.theme === 'light' && 'is-active'">
+        <Icon name="sun" />
+        <span>Light</span>
+      </button>
+    </li>
+    <li class="menu__item">
+      <button @click="changeTheme('dark')" class="menu__action" :class="store.theme === 'dark' && 'is-active'">
+        <Icon name="moon" />
+        <span>Dark</span>
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import Icon from './Icon.vue';
+import { store } from '../modules/useThemeStore';
+
+function changeTheme(value) {
+  store.change(value);
+}
+</script>
---
+++ docs/src/components/ThemeSwitcherPopover.vue
@@ -0,0 +1,24 @@
+<template>
+  <Popover
+    id="theme-switcher"
+    aria-label="Switch selected theme"
+    trigger-class="button button_icon"
+    target-class="popover_size_auto"
+    placement="bottom-end">
+    <template #trigger>
+      <Icon v-if="store.theme === 'default'" name="box" />
+      <Icon v-if="store.theme === 'light'" name="sun" />
+      <Icon v-if="store.theme === 'dark'" name="moon" />
+    </template>
+    <template #content>
+      <ThemeSwitcherMenu />
+    </template>
+  </Popover>
+</template>
+
+<script setup>
+import Popover from './Popover.vue';
+import Icon from './Icon.vue';
+import ThemeSwitcherMenu from './ThemeSwitcherMenu.vue';
+import { store } from '../modules/useThemeStore';
+</script>
