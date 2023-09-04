adc2feb556770eb904e8f6eaeac565c2ce0698c9
sebnitu <nitu.seb@gmail.com>
2023-09-04 10:33:49 -0700

    Fix header anchor position

--- docs/src/modules/headerAnchor.js
+++ docs/src/modules/headerAnchor.js
@@ -1,7 +1,7 @@
 function headerAnchor(options = {}) {
   const settings = {
-    prefix: ".layout__content",
-    headings: ["h1", "h2", "h3", "h4", "h5", "h6"],
+    prefix: '.layout__content',
+    headings: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
     ...options
   };
 
@@ -11,13 +11,13 @@ function headerAnchor(options = {}) {
       els.forEach((el) => {
         el.setAttribute('tabindex', '-1');
         const elText = el.innerHTML;
-        const elId = el.getAttribute("id");
-        const anchor = document.createElement("a");
-        anchor.setAttribute("class", "header-anchor");
-        anchor.setAttribute("href", "#" + elId);
-        anchor.setAttribute("aria-label", `Permalink to "${elText}"`);
-        anchor.innerHTML = "#";
-        el.append(anchor);
+        const elId = el.getAttribute('id');
+        const anchor = document.createElement('a');
+        anchor.setAttribute('class', 'header-anchor');
+        anchor.setAttribute('href', '#' + elId);
+        anchor.setAttribute('aria-label', `Permalink to '${elText}'`);
+        anchor.innerHTML = '#';
+        el.prepend(anchor);
       });
     });
   }
