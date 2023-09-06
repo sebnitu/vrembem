89b15f0978f32caa6c3892addcaed001344a177a
sebnitu <nitu.seb@gmail.com>
2023-09-06 10:17:46 -0700

    Refactor useThemeStore to work in SSR

--- docs/src/modules/useThemeStore.js
+++ docs/src/modules/useThemeStore.js
@@ -1,24 +1,30 @@
 import { reactive } from 'vue';
 import { localStore } from '@vrembem/core';
 
-const profile = localStore('VB:Profile');
-
 const prefix = 'theme-';
 const themes = ['light', 'dark', 'root'];
 const classes = themes.map((theme) => `${prefix}${theme}`);
 
 const store = reactive({
-  theme: profile.get('theme'),
+  theme: 'root',
   change(value) {
+    console.error(`Document is not available in this context: ${value}`);
+  }
+});
+
+if (typeof window !== 'undefined') {
+  const profile = localStore('VB:Profile');
+  store.theme = profile.get('theme') || 'root';
+  store.change = (value) => {
     if (themes.includes(value)) {
-      this.theme = value;
+      store.theme = value;
       profile.set('theme', value);
       document.documentElement.classList.remove(...classes);
       document.documentElement.classList.add(`${prefix}${value}`);
     } else {
       console.error(`Not a valid theme: "${value}"`);
     }
-  }
-});
+  };
+}
 
 export { store };
