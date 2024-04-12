b5c8c23c080ec677837e83e6a9f032272e7bd287
Sebastian Nitu <nitu.seb@gmail.com>
2024-04-12 09:28:51 -0700

    Refactor card component and apply new docs format (#1769)
    This PR refactors the card component and applies the usage of the theme module. It also deprecates the following card modifiers:
    
    - `card_fade`
    - `card_link`
    - `card_zoom`
    
    Documentation has been re-written to reflect these changes.

--- docs/src/modules/headerAnchor.js
+++ docs/src/modules/headerAnchor.js
@@ -9,7 +9,13 @@ function headerAnchor(options = {}) {
     settings.headings.forEach((heading) => {
       const els = document.querySelectorAll(`${settings.prefix} ${heading}`);
       els.forEach((el) => {
+        // Return if heading has the `no-anchor` class.
         if (el.classList.contains('no-anchor')) return;
+        
+        // Return if heading is in a code example.
+        if (el.closest('.code-example')) return;
+
+        // Setup the heading anchor.
         el.setAttribute('tabindex', '-1');
         const elText = el.innerHTML;
         const elId = el.getAttribute('id');
