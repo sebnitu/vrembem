4f14af3b3cd4ca51e0db9ac31b44dbe45d30d102
Sebastian Nitu <nitu.seb@gmail.com>
2024-05-06 16:50:04 -0700

    Update checkbox documentation and expanded documentation features (#1788)
    This PR contains a complete rewrite of the Checkbox documentation and uses the new docs format. This PR also updates the values of Checkbox's variables to properly use the Palette module for applying hover, focus and active states. Lastly, the Palette module `get` function no longer returns a CSS variable fallback with the `var` method.
    
    **Changes to CodeExample**
    
    Code examples have been expanded to use tabs for displaying multiple language applications for an example. Currently supports an `html`, `scss` and `js` example per CodeExample wrapper. Tabs are enabled using the `tabs` attribute and applied using slots. The default slot is not used when tabs are enabled and all examples should be applied via a slot.
    
    ```html
    <CodeExample tabs={true}>
      <Fragment slot="output">
        ...
      </Fragment>
      <Fragment slot="tab-html">
        ...
      </Fragment>
      <Fragment slot="tab-scss">
        ...
      </Fragment>
      <Fragment slot="tab-js">
        ...
      </Fragment>
    </CodeExample>
    ```
    
    **Changes to DocBlock**
    
    CodeBlock now allows the use of `returns` and `line` attributes for rendering what a function returns as well as a specific line number in the `src` link respectively. DocBlock styles have also been updated to support light/dark mode in respect block highlighting.
    
    ```html
    <DocBlock
      name="function-name"
      type="function"
      returns="string"
      src="functions.scss"
      line="9"
      args={[
        ["$asdf", "Number", "..."],
        ["$fdsa", "Boolean", "..."]
      ]}
    >
      ...
    
      <Fragment slot="example">
        ...
      </Fragment>
    </DocBlock>
    ```

---
+++ docs/src/modules/tabs.js
@@ -0,0 +1,81 @@
+function selectTab(active, tabs) {
+  tabs.forEach((tab) => {
+    const tabpanel = document.querySelector(`#${tab.getAttribute('aria-controls')}`);
+    if (active === tab) {
+      tab.setAttribute('aria-selected', 'true');
+      tab.removeAttribute('tabindex');
+      tabpanel.classList.add('is-active');
+    } else {
+      tab.setAttribute('aria-selected', 'false');
+      tab.setAttribute('tabindex', '-1');
+      tabpanel.classList.remove('is-active');
+    }
+  });
+}
+
+function selectNextTab(tabs) {
+  let nextTab = document.activeElement.nextElementSibling;
+  nextTab = nextTab ? nextTab : tabs[0];
+  selectTab(nextTab, tabs);
+  nextTab.focus();
+}
+
+function selectPrevTab(tabs) {
+  let prevTab = document.activeElement.previousElementSibling;
+  prevTab = prevTab ? prevTab : tabs[tabs.length - 1];
+  selectTab(prevTab, tabs);
+  prevTab.focus();
+}
+
+function handlerKeydown(tabs, event) {
+  switch (event.key) {
+    case 'Down':
+    case 'ArrowDown':
+    case 'Right':
+    case 'ArrowRight':
+      event.preventDefault();
+      selectNextTab(tabs);
+      break;
+    case 'Up':
+    case 'ArrowUp':
+    case 'Left':
+    case 'ArrowLeft':
+      event.preventDefault();
+      selectPrevTab(tabs);
+      break;
+    default:
+      return;
+  }
+}
+
+const tabs = {
+  mount() {
+    // Get all the tablists.
+    const tablists = document.querySelectorAll('[role="tablist"]');
+    tablists.forEach((tablist) => {
+      const tabs = tablist.querySelectorAll('[role="tab"]');
+
+      // Select the first tab.
+      selectTab(tabs[0], tabs);
+
+      // Setup event listeners for tabs.
+      tabs.forEach((tab) => {
+        const __handlerKeydown = handlerKeydown.bind(null, tabs);
+
+        tab.addEventListener('click', () => {
+          selectTab(tab, tabs);
+        });
+
+        tab.addEventListener('focus', () => {
+          document.addEventListener('keydown', __handlerKeydown);
+        });
+
+        tab.addEventListener('blur', () => {
+          document.removeEventListener('keydown', __handlerKeydown);
+        });
+      });
+    });
+  }
+};
+
+export { tabs };
