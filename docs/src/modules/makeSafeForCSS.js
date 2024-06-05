8f75e988d80e57ab1494bc44d88f746f95cb4dcc
Sebastian Nitu <nitu.seb@gmail.com>
2024-06-05 10:17:26 -0700

    Migrate drawer, modal and popover documentation to new format (#1849)
    This PR applies the new documentation format to the modal and popover components. Also included are some minor tweaks and fixes to the drawer docs. Other general documentation improvements include:
    
    - Made "On this page" title a clickable "main" anchor.
    - "On this page" menu now displays `h2` and `h3` anchors.
    - Added examples directory to put reusable code examples (such as menus used in popover docs).
    - Used `String.trim()` more consistently in component class usage.
    - Wrapped `DocBlock` slot with type module.

--- docs/src/modules/makeSafeForCSS.js
+++ docs/src/modules/makeSafeForCSS.js
@@ -2,6 +2,7 @@ function makeSafeForCSS(name) {
   return name.replace(/[^a-z0-9]/g, function(s) {
     const c = s.charCodeAt(0);
     if (c == 32) return "-";
+    if (c == 46) return "-";
     return s.toLowerCase();
   });
 }
