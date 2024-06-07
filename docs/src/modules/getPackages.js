97b3f9c8c06faf32ba76e686044bdf97d5489326
sebnitu <nitu.seb@gmail.com>
2024-06-07 09:15:13 -0700

    Update the use of state to status in getPackages

--- docs/src/modules/getPackages.js
+++ docs/src/modules/getPackages.js
@@ -1,7 +1,7 @@
 import { getCollection } from "astro:content";
 
 function packageOrder(a, b) {
-  return a.data.state == "settings" ? -1 : b.data.state == "settings" ? 1 : (a.data.state == "layers" ? -1 : b.data.state == "layers" ? 1 : 0);
+  return a.data.status == "settings" ? -1 : b.data.status == "settings" ? 1 : (a.data.status == "layers" ? -1 : b.data.status == "layers" ? 1 : 0);
 }
 
 export async function getPackages() {
