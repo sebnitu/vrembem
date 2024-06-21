a3eb41b27fcf42291f47625d63fddcc2dca36c52
sebnitu <nitu.seb@gmail.com>
2024-06-20 18:06:31 -0700

    Allow passing multiple classes in demo input values

--- docs/src/modules/demo.js
+++ docs/src/modules/demo.js
@@ -8,7 +8,8 @@ demos.forEach((demo) => {
   if (output && input) {
     // Set the default selection.
     for (let i = 0; i < demo.options.length; i++) {
-      if (output.classList.contains(demo.options[i].value)) {
+      let classes = demo.options[i].value.split(" ");
+      if (demo.options[i].text != "None" && output.classList.contains(classes.at(-1))) {
         demo.selectedIndex = i;
         break;
       }
@@ -20,8 +21,11 @@ demos.forEach((demo) => {
 
       // Loop through all the possible demo options.
       for (let i = 0; i < demo.options.length; i++) {
-        // Remove the classes from the output.
-        output.classList.remove(demo.options[i].value);
+        // Convert the options value into classes list.
+        let classes = demo.options[i].value.split(" ");
+
+        // Remove the last class from the output.
+        output.classList.remove(classes.at(-1));
 
         // Store the old value if it exists in the input html.
         if (input.innerHTML.includes(demo.options[i].value)) {
@@ -32,7 +36,7 @@ demos.forEach((demo) => {
       };
 
       // Update the output and input with new values.
-      output.classList.add(demo.value);
+      output.classList.add(...demo.value.split(" "));
       input.innerHTML = input.innerHTML.replace(oldValue, demo.value);
     });
   }
