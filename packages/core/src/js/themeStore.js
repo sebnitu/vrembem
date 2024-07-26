6a36e46fb74865705027fbfdf4a10e156b2bbb39
sebnitu <nitu.seb@gmail.com>
2024-07-26 15:42:58 -0700

    Refactor themeStore and improve the API

--- packages/core/src/js/themeStore.js
+++ packages/core/src/js/themeStore.js
@@ -1,27 +1,55 @@
 import { localStore } from "@vrembem/core";
 
-const prefix = "vb-theme-";
-const themes = ["light", "dark", "root"];
-const classes = themes.map((theme) => `${prefix}${theme}`);
+export function themeStore(options) {
+  const settings = {
+    prefix: "vb-theme-",
+    themes: ["root", "light", "dark"],
+    storeKey: "VB:Profile",
+    onInit() {},
+    onChange() {},
+    ...options
+  };
 
-const store = {
-  theme: "root",
-  change(value) {
-    console.error(`Document is not available in this context: ${value}`);
-  }
-};
+  const profile = localStore(settings.storeKey);
 
-const profile = localStore("VB:Profile");
-store.theme = profile.get("theme") || "root";
-store.change = (value) => {
-  if (themes.includes(value)) {
-    store.theme = value;
-    profile.set("theme", value);
-    document.documentElement.classList.remove(...classes);
-    document.documentElement.classList.add(`${prefix}${value}`);
-  } else {
-    console.error(`Not a valid theme: "${value}"`);
-  }
-};
+  const api = {
+    settings,
+    onInit() { this.settings.onInit.call(this); },
+    onChange() { this.settings.onChange.call(this); },
+    add(value) {
+      return this.settings.themes.push(value);
+    },
+    remove(value) {
+      const index = this.settings.themes.indexOf(value);
+      return (~index) ? this.settings.themes.splice(index, 1) : this.settings.themes;
+    },
+    get class() {
+      return `${this.settings.prefix}${this.theme}`;
+    },
+    get classes() {
+      return this.settings.themes.map((theme) => `${this.settings.prefix}${theme}`);
+    },
+    get theme() {
+      return profile.get("theme") || "root";
+    },
+    set theme(value) {
+      if (this.themes.includes(value)) {
+        if (this.theme != value) {
+          profile.set("theme", value);
+          document.documentElement.classList.remove(...this.classes);
+          document.documentElement.classList.add(`${this.settings.prefix}${value}`);
+          this.onChange();
+        }
+      } else {
+        console.error(`Not a valid theme value: "${value}"`);
+      }
+    },
+    get themes() {
+      return this.settings.themes;
+    },
+  };
 
-export { store as themeStore };
+  api.onInit();
+
+  return api;
+}
