ba6beada47328f1decacbc272cc17c536ad8fcca
sebnitu <nitu.seb@gmail.com>
2024-07-25 23:32:52 -0700

    Initial implementation of themeStore and theme switching

---
+++ packages/core/src/js/themeStore.js
@@ -0,0 +1,27 @@
+import { localStore } from "@vrembem/core";
+
+const prefix = "vb-theme-";
+const themes = ["light", "dark", "root"];
+const classes = themes.map((theme) => `${prefix}${theme}`);
+
+const store = {
+  theme: "root",
+  change(value) {
+    console.error(`Document is not available in this context: ${value}`);
+  }
+};
+
+const profile = localStore("VB:Profile");
+store.theme = profile.get("theme") || "root";
+store.change = (value) => {
+  if (themes.includes(value)) {
+    store.theme = value;
+    profile.set("theme", value);
+    document.documentElement.classList.remove(...classes);
+    document.documentElement.classList.add(`${prefix}${value}`);
+  } else {
+    console.error(`Not a valid theme: "${value}"`);
+  }
+};
+
+export { store as themeStore };
