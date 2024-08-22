0fbd9f7d26974d86feb222865fe8465c50df5d9e
sebnitu <nitu.seb@gmail.com>
2024-08-22 09:09:36 -0700

    Update demo and vite config to use package data

--- packages/popover/vite.config.js
+++ packages/popover/vite.config.js
@@ -1,5 +1,8 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import data from "./package.json";
+
+const parts = data.name.replace("@", "").split("/");
 
 export default defineConfig({
   build: {
@@ -12,6 +15,9 @@ export default defineConfig({
     sourcemap: true
   },
   define: {
-    "import.meta.env.VITE_PACKAGE": JSON.stringify("popover"),
+    "import.meta.env.VITE_ROOT": JSON.stringify(parts[0]),
+    "import.meta.env.VITE_PACKAGE": JSON.stringify(parts[1]),
+    "import.meta.env.VITE_NAME": JSON.stringify(data.name),
+    "import.meta.env.VITE_DESCRIPTION": JSON.stringify(data.description)
   }
 });
