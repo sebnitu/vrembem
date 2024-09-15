324500a4f4e4474478e997ba181609219fbbb878
sebnitu <nitu.seb@gmail.com>
2024-09-15 11:44:42 -0700

    Add default value to getConfig dataConfig param

--- packages/core/src/js/getConfig.js
+++ packages/core/src/js/getConfig.js
@@ -1,4 +1,4 @@
-export function getConfig(el, dataConfig) {
+export function getConfig(el, dataConfig = "config") {
   const string = el.getAttribute(`data-${dataConfig}`) || "";
   const json = string.replace(/'/g, "\"");
   return (json) ? JSON.parse(json) : {};
