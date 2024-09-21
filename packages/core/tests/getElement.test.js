0d68a79259af6ca2191e9ece08620c7c6ea2b08b
sebnitu <nitu.seb@gmail.com>
2024-09-21 10:45:48 -0700

    Add test coverage for getElement

---
+++ packages/core/tests/getElement.test.js
@@ -0,0 +1,19 @@
+import { getElement } from "../src/js/getElement";
+
+document.body.innerHTML = `
+  <div id="asdf"></div>
+`;
+
+test("should return an HTML element with the provided ID", () => {
+  const el = getElement("asdf");
+  expect(el instanceof HTMLElement).toBe(true);
+});
+
+test("should return an HTML element if an HTML element is provided", () => {
+  const el = document.getElementById("asdf");
+  expect(getElement(el)).toEqual(el);
+});
+
+test("should return undefined if the provided argument isn't valid", () => {
+  expect(getElement(64)).toBe(undefined);
+});
