cb59ee53103ce63974726dca3ba0e92d0f926ed1
sebnitu <nitu.seb@gmail.com>
2024-09-27 12:12:56 -0700

    Initial setup of test directory

---
+++ test/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    emptyOutDir: false,
+    sourcemap: true
+  },
+  css: {
+    preprocessorOptions : {
+      scss: {
+        api: "modern",
+      }        
+    } 
+  }
+});
