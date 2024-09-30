af87582d8a98a778c4d84b8641412bca6b15dc99
sebnitu <nitu.seb@gmail.com>
2024-09-30 09:39:07 -0700

    Use camel case in getSetting call

--- packages/core/src/js/helpers/getCustomProps.js
+++ packages/core/src/js/helpers/getCustomProps.js
@@ -8,7 +8,7 @@ export function getCustomProps(entry) {
   const result = {};
 
   // Get the custom property keys.
-  const keys = entry.getSetting("custom-props");
+  const keys = entry.getSetting("customProps");
 
   // Loop through the custom properties object.
   for (let i = 0; i < keys.length; i++) {
