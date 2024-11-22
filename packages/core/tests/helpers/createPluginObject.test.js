441161306b6f25afd9415d6cf4da2c5ebf97a1ce
sebnitu <nitu.seb@gmail.com>
2024-11-22 14:45:33 -0800

    Fix tests for refactored createPluginObject

--- packages/core/tests/helpers/createPluginObject.test.js
+++ packages/core/tests/helpers/createPluginObject.test.js
@@ -2,55 +2,51 @@ import { createPluginObject } from "../../index";
 
 console.error = vi.fn();
 
-const props = {
+const obj = {
   name: "example",
-  settings: {
+  config: {
     value: true,
     string: "asdf"
-  }
-};
-
-const methods = {
+  },
   beforeMount() {},
   beforeUnmount() {}
 };
 
-test("should create and return a plugin object", () => {
-  const plugin = createPluginObject(props, methods);
-  expect(plugin.name).toBe("example");
-  expect(typeof plugin.beforeMount).toBe("function");
-  expect(typeof plugin.beforeUnmount).toBe("function");
-  expect(plugin.settings.string).toBe("asdf");
-  expect(plugin.settings.value).toBe(true);
-});
-
-test("should be able to pass lifecycle hooks and name through settings", () => {
-  const settings = {
-    name: "newExample",
-    onRegisterEntry() {}
-  };
-  props.settings = {...props.settings, ...settings};
-  const plugin = createPluginObject(props, methods);
-  expect(plugin.name).toBe("newExample");
-  expect(typeof plugin.onRegisterEntry).toBe("function");
-});
-
-test("should allow overriding lifecycle hooks that have already been defined if override is set to true", () => {
-  const settings = {
-    override: true,
-    onMount() {}
-  };
-  props.settings = {...props.settings, ...settings};
-  createPluginObject(props, methods);
-  expect(console.error).not.toHaveBeenCalled();
-});
-
-test("should log console error if setting a lifecycle hook that has already been defined", () => {
-  const settings = {
-    override: false,
-    beforeMount() {}
-  };
-  props.settings = {...props.settings, ...settings};
-  createPluginObject(props, methods);
-  expect(console.error).toBeCalledWith("newExample plugin already has \"beforeMount\" lifecycle hook defined!");
+describe("createPluginObject", () => {
+  it("should create and return a plugin object", () => {
+    const plugin = createPluginObject(obj);
+    expect(plugin.name).toBe("example");
+    expect(typeof plugin.beforeMount).toBe("function");
+    expect(typeof plugin.beforeUnmount).toBe("function");
+    expect(plugin.settings.string).toBe("asdf");
+    expect(plugin.settings.value).toBe(true);
+  });
+  
+  it("should be able to pass lifecycle hooks and name through settings", () => {
+    obj.config = {
+      name: "newExample",
+      onRegisterEntry() {}
+    };
+    const plugin = createPluginObject(obj);
+    expect(plugin.name).toBe("newExample");
+    expect(typeof plugin.onRegisterEntry).toBe("function");
+  });
+  
+  it("should allow overriding lifecycle hooks that have already been defined if override is set to true", () => {
+    obj.config = {
+      override: true,
+      onMount() {}
+    };
+    createPluginObject(obj);
+    expect(console.error).not.toHaveBeenCalled();
+  });
+  
+  it("should log console error if setting a lifecycle hook that has already been defined", () => {
+    obj.config = {
+      override: false,
+      beforeMount() {}
+    };
+    createPluginObject(obj);
+    expect(console.error).toBeCalledWith("newExample plugin already has \"beforeMount\" lifecycle hook defined!");
+  });  
 });
