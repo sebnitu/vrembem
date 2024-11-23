f7e97fef3ad15717dfe837ba36d56c5c6927df85
sebnitu <nitu.seb@gmail.com>
2024-11-22 16:25:47 -0800

    Fix pluginsArray tests and remove dead validation check

--- packages/core/src/js/helpers/pluginsArray.js
+++ packages/core/src/js/helpers/pluginsArray.js
@@ -8,11 +8,6 @@ export class pluginsArray extends Array {
   }
 
   validate(plugin) {
-    if (typeof plugin != "object") {
-      console.error("Plugin is not a valid object!");
-      return false;
-    };
-
     if (!("name" in plugin) || typeof plugin.name !== "string") {
       console.error("Plugin requires a name!");
       return false;
--- packages/core/tests/helpers/pluginsArray.test.js
+++ packages/core/tests/helpers/pluginsArray.test.js
@@ -1,48 +1,51 @@
 import { pluginsArray } from "../../src/js/helpers/pluginsArray";
 
-const plugins = new pluginsArray({});
-
 console.error = vi.fn();
 
-test("should add a plugin to the plugins array", () => {
-  expect(plugins.length).toBe(0);
-  plugins.add({ name: "asdf" });
-  expect(plugins.length).toBe(1);
+const plugins = new pluginsArray({
+  module: "Example"
 });
 
-test("should remove a plugin from the plugins array", async () => {
-  expect(plugins.length).toBe(1);
-  await plugins.remove("asdf");
-  expect(plugins.length).toBe(0);
-});
+describe("pluginsArray", () => {
+  it("should add a plugin to the plugins array", () => {
+    expect(plugins.length).toBe(0);
+    plugins.add({ name: "asdf" });
+    expect(plugins.length).toBe(1);
+  });
+  
+  it("should remove a plugin from the plugins array", async () => {
+    expect(plugins.length).toBe(1);
+    await plugins.remove("asdf");
+    expect(plugins.length).toBe(0);
+  });
+  
+  it("should add an array of plugins at once", () => {
+    expect(plugins.length).toBe(0);
+    plugins.add([
+      { name: "one", config: { a: "a" } },
+      { name: "two", config: { b: "b" } }
+    ]);
+    expect(plugins.length).toBe(2);
+  });
+  
+  it("should return a specific plugin when using the get method", () => {
+    const pluginOne = plugins.get("one");
+    expect(pluginOne.settings).toStrictEqual({ a: "a" });
 
-test("should add an array of plugins at once", () => {
-  expect(plugins.length).toBe(0);
-  plugins.add([
-    { name: "one", settings: { a: "a" } },
-    { name: "two", settings: { b: "b" } }
-  ]);
-  expect(plugins.length).toBe(2);
-});
+    const pluginTwo = plugins.get("two");
+    expect(pluginTwo.settings).toStrictEqual({ b: "b" });
 
-test("should return a specific plugin when using the get method", () => {
-  expect(plugins.get("one")).toStrictEqual({ name: "one", settings: { a: "a" } });
-  expect(plugins.get("two")).toStrictEqual({ name: "two", settings: { b: "b" } });
-  expect(plugins.get("three")).toBe(undefined);
-});
-
-test("should log a console error if the plugin is not a valid object", () => {
-  plugins.add("asdf");
-  expect(console.error).toBeCalledWith("Plugin is not a valid object!");
-});
-
-test("should log a console error if the plugin is missing a name property", () => {
-  plugins.add({});
-  expect(console.error).toBeCalledWith("Plugin requires a name!");
-});
-
-test("should log a console error if the plugin name is already used", () => {
-  plugins.add({ name: "asdf" });
-  plugins.add({ name: "asdf" });
-  expect(console.error).toBeCalledWith("Plugin with the name \"asdf\" is already being used!");
+    expect(plugins.get("three")).toBe(undefined);
+  });
+  
+  it("should log a console error if the plugin is missing a name property", () => {
+    plugins.add({});
+    expect(console.error).toBeCalledWith("Plugin requires a name!");
+  });
+  
+  it("should log a console error if the plugin name is already used", () => {
+    plugins.add({ name: "asdf" });
+    plugins.add({ name: "asdf" });
+    expect(console.error).toBeCalledWith("Plugin with the name \"asdf\" is already being used!");
+  });  
 });
