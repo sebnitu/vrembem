8200d06fce244bb149940342441f1fd09e197077
sebnitu <nitu.seb@gmail.com>
2025-06-09 22:25:17 -0700

    Setup tsc to output .d.ts files

--- packages/core/src/js/utilities/getDataConfig.ts
+++ packages/core/src/js/utilities/getDataConfig.ts
@@ -1,19 +1,19 @@
 /**
- * Get and parses a JSON object from a `data-*` attribute of an HTML element.
+ * Gets and parses a JSON object from a `data-*` attribute of an HTML element.
  *
- * @param {HTMLElement} el
- *   Element to get the query the data from.
- * @param {string} [dataConfig="config"]
- *   The name of the data attribute to get.
+ * @param el
+ *   Element to get the data from.
+ * @param dataConfig
+ *   The name of the data attribute to get. Defaults to "config".
  *
- * @returns {JSON}
- *   A parsed JSON object from the `data-*` attribute, or an empty object
+ * @returns
+ *   A parsed object from the `data-*` attribute, or an empty object
  *   if the attribute is not found or invalid.
  */
 export function getDataConfig(
   el: HTMLElement,
   dataConfig: string = "config"
-): JSON {
+): Record<string, any> {
   const string = el.getAttribute(`data-${dataConfig}`) || "";
   const json = string.replace(/'/g, '"');
   return json ? JSON.parse(json) : {};
