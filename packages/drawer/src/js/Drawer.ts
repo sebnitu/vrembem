d34b955a1f2cf4827a2587532a5d22a590b89614
sebnitu <nitu.seb@gmail.com>
2025-06-11 08:35:47 -0700

    Convert Drawer.js to ts

--- packages/drawer/src/js/Drawer.ts
+++ packages/drawer/src/js/Drawer.ts
@@ -8,10 +8,10 @@ import { close } from "./close";
 import { toggle } from "./toggle";
 
 export class Drawer extends Collection {
-  #handleClick;
-  #handleKeydown;
+  #handleClick: (event: Event) => void;
+  #handleKeydown: (event: KeyboardEvent) => void;
 
-  constructor(options) {
+  constructor(options: Record<string, any>) {
     super({ ...defaults, ...options });
     this.module = "Drawer";
     this.entryClass = DrawerEntry;
@@ -20,30 +20,26 @@ export class Drawer extends Collection {
   }
 
   get activeModal() {
-    return this.collection.find((entry) => {
+    return this.collection.find((entry: DrawerEntry) => {
       return entry.state === "opened" && entry.mode === "modal";
     });
   }
 
-  async open(id, transition, focus) {
+  async open(id: string, transition?: boolean, focus?: boolean) {
     return open.call(this, id, transition, focus);
   }
 
-  async close(id, transition, focus) {
+  async close(id: string, transition?: boolean, focus?: boolean) {
     return close.call(this, id, transition, focus);
   }
 
-  async toggle(id, transition, focus) {
+  async toggle(id: string, transition?: boolean, focus?: boolean) {
     return toggle.call(this, id, transition, focus);
   }
 
   async afterMount() {
     document.addEventListener("click", this.#handleClick, false);
     document.addEventListener("keydown", this.#handleKeydown, false);
-  }
-
-  async beforeUnmount() {
-    this.trigger = null;
   }
 
   async afterUnmount() {
