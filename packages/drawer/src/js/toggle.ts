45ade42b32f4155d19e6e4812373ea94d34a4a3e
sebnitu <nitu.seb@gmail.com>
2025-06-16 15:52:16 -0700

    Convert drawer toggle to typescript

--- packages/drawer/src/js/toggle.ts
+++ packages/drawer/src/js/toggle.ts
@@ -1,10 +1,17 @@
 import { open } from "./open";
 import { close } from "./close";
 import { getDrawer } from "./helpers/getDrawer";
+import type { Drawer } from "./Drawer";
+import type { DrawerEntry } from "./DrawerEntry";
 
-export async function toggle(query, transition, focus) {
+export async function toggle(
+  this: Drawer,
+  query: string | HTMLElement,
+  transition: boolean,
+  focus: boolean
+): Promise<DrawerEntry> {
   // Get the drawer from collection
-  const entry = getDrawer.call(this, query);
+  const entry: DrawerEntry = getDrawer.call(this, query);
 
   // Open or close the drawer based on its current state
   if (entry.state === "closed") {
