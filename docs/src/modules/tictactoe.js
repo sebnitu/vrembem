296f039e534bf3273f6dcef19435362c38b335d9
sebnitu <nitu.seb@gmail.com>
2025-12-05 16:52:56 -0800

    Add custom collection entry example to docs

--- docs/src/modules/tictactoe.js
+++ docs/src/modules/tictactoe.js
@@ -1,9 +1,17 @@
-import { Collection } from "@vrembem/core";
+import { Collection, CollectionEntry } from "@vrembem/core";
+
+// Define a custom collection entry class
+class TicTacToeCell extends CollectionEntry {
+  constructor(...args) {
+    super(...args);
+  }
+}
 
 // Define our custom collection class
 class TicTacToe extends Collection {
   constructor(options) {
     super(options);
+    this.entryClass = TicTacToeCell;
   }
 }
 
