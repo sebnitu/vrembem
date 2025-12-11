8866b79a2071431475d93e31184a6337ba4cb6f3
sebnitu <nitu.seb@gmail.com>
2025-12-10 23:59:19 -0800

    Remove getConfig from helpers

--- packages/core/src/js/helpers/index.ts
+++ packages/core/src/js/helpers/index.ts
@@ -1,6 +1,5 @@
 export * from "./cssVar";
 export * from "./dispatchLifecycleHook";
-export * from "./getConfig";
 export * from "./getCustomProps";
 export * from "./getPrefix";
 export * from "./setGlobalState";
