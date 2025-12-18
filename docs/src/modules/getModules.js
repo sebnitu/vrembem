61f628e7b8ea3b952df51beb31c7a934b0b763f6
sebnitu <nitu.seb@gmail.com>
2025-12-17 17:22:38 -0800

    Update sort logic of getModules

--- docs/src/modules/getModules.js
+++ docs/src/modules/getModules.js
@@ -1,8 +1,6 @@
 import { getCollection } from "astro:content";
 
 function byTitle(a, b) {
-  if (a.data.title === "Custom Plugins") return 1;
-  if (b.data.title === "Custom Plugins") return -1;
   if (a.data.title < b.data.title) return -1;
   if (a.data.title > b.data.title) return 1;
   return 0;
@@ -11,10 +9,10 @@ function byTitle(a, b) {
 function byOrder(a, b) {
   const aOrder = typeof a.data.order === "number" ? a.data.order : null;
   const bOrder = typeof b.data.order === "number" ? b.data.order : null;
-  if (aOrder !== null && bOrder !== null) return aOrder - bOrder;
-  if (aOrder !== null) return -1;
-  if (bOrder !== null) return 1;
-  return 0;
+  if (aOrder === null && bOrder === null) return 0;
+  if (aOrder === null) return -1;
+  if (bOrder === null) return 1;
+  return aOrder - bOrder;
 }
 
 export async function getModules(group) {
