b5736b35c8637729d2ac436b2cb00703f9660120
sebnitu <nitu.seb@gmail.com>
2026-03-25 16:32:47 -0700

    Add popovers to menu and modal close buttons

--- docs/src/modules/usePopover.js
+++ docs/src/modules/usePopover.js
@@ -7,6 +7,15 @@ if (typeof window !== "undefined") {
   popover = new Popover({
     plugins: [cssConfig(), teleport({ where: ".popovers" })]
   });
+
+  // Return modal popover to prevent stacking issue
+  popover.on("teleport", ({ entry }) => {
+    if (entry.id === "popover-tooltip-modal-navi-close") {
+      console.log(entry);
+      entry.teleportReturn();
+    }
+  });
+
   window["popover"] = await popover.mount();
 }
 
