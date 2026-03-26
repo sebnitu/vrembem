d681f62ea71ce97c72b3ff0a70014645554145c5
Sebastian Nitu <nitu.seb@gmail.com>
2026-03-25 17:05:52 -0700

    Refactor documentation layout component (#2724)
    This PR primarily refactors the layout component for the documentation website. It aligns the styles of the layout sidebar and layout aside to better share their style treatment. As part of this update, layout markup and styles have been reduced substantially.
    
    **Additional changes**
    
    - `docs`: Renamed layout-drawer to layout-sidebar to better aligned with new layout styles.
    - `docs`: Update footer styles and markup to match new layout styles.
    - `docs`: Removed `code` breakpoint in favor of using `xs` instead.
    - `core`: Renamed the `prefix-cssvars` config option to `prefix-tokens`.

--- docs/src/modules/usePopover.js
+++ docs/src/modules/usePopover.js
@@ -1,12 +1,21 @@
 import { Popover } from "vrembem";
-import { cssConfig } from "@vrembem/core";
+import { cssConfig, teleport } from "@vrembem/core";
 
 let popover = null;
 
 if (typeof window !== "undefined") {
   popover = new Popover({
-    plugins: [cssConfig()]
+    plugins: [cssConfig(), teleport({ where: ".popovers" })]
   });
+
+  // Return modal popover to prevent stacking issue
+  popover.on("teleport", ({ entry }) => {
+    if (entry.id === "popover-tooltip-modal-navi-close") {
+      console.log(entry);
+      entry.teleportReturn();
+    }
+  });
+
   window["popover"] = await popover.mount();
 }
 
