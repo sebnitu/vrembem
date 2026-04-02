b38a9b6d0bc1f62b9ac1b2fa80cf9d2b0770cdf7
sebnitu <nitu.seb@gmail.com>
2026-04-02 16:49:56 -0700

    Convert EventEmitter to use a map instead of a generic object

--- packages/core/src/js/modules/EventEmitter.ts
+++ packages/core/src/js/modules/EventEmitter.ts
@@ -5,29 +5,25 @@ export interface EventEntry {
   args: any[];
 }
 
-export interface EventMap {
-  [event: string]: EventEntry[];
-}
-
 export class EventEmitter {
-  readonly events: EventMap = {};
+  readonly events = new Map<string, EventEntry[]>();
 
   on(event: string, listener: EventListener, ...args: any[]): void {
-    if (!this.events[event]) {
-      this.events[event] = [];
+    if (!this.events.has(event)) {
+      this.events.set(event, []);
     }
-    const listenerExists = this.events[event].some(
-      (entry) => entry.listener === listener
-    );
-    if (!listenerExists) {
-      this.events[event].push({ listener, args });
+    const entries = this.events.get(event)!;
+    if (!entries.some((entry) => entry.listener === listener)) {
+      entries.push({ listener, args });
     }
   }
 
   off(event: string, listenerRef: EventListener): void {
-    if (!this.events[event]) return;
-    this.events[event] = this.events[event].filter(
-      (entry) => entry.listener !== listenerRef
+    const entries = this.events.get(event);
+    if (!entries) return;
+    this.events.set(
+      event,
+      entries.filter((entry) => entry.listener !== listenerRef)
     );
   }
 
@@ -35,8 +31,9 @@ export class EventEmitter {
     event = event.startsWith("on")
       ? event.slice(2, 3).toLowerCase() + event.slice(3)
       : event;
-    if (!this.events[event]) return;
-    for (const { listener, args: listenerArgs } of this.events[event]) {
+    const entries = this.events.get(event);
+    if (!entries) return;
+    for (const { listener, args: listenerArgs } of entries) {
       await listener(...args, ...listenerArgs);
     }
   }
