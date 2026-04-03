a0beea878ce4cc5231934e9541f676a7374891ce
sebnitu <nitu.seb@gmail.com>
2026-04-03 11:02:02 -0700

    chore: update stylelint config to resolve warnings

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,9 +1,24 @@
+import { propertyGroups } from "stylelint-config-clean-order";
+
+const propertiesOrder = propertyGroups.map((properties) => ({
+  noEmptyLineBetween: true,
+  emptyLineBefore: "never",
+  properties
+}));
+
 export default {
   extends: ["stylelint-config-clean-order", "stylelint-config-standard-scss"],
   plugins: ["stylelint-prettier"],
   ignoreFiles: ["**/dev/**/*", "**/dist/**/*"],
   rules: {
     "prettier/prettier": true,
+    "order/properties-order": [
+      propertiesOrder,
+      {
+        severity: "warning",
+        unspecified: "bottomAlphabetical"
+      }
+    ],
     "function-url-quotes": null,
     "selector-class-pattern": [
       "^[a-z0-9-_:]+$",
