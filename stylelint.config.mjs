99a624d6b74f4dab1fa79509775425dd5b76a51c
sebnitu <nitu.seb@gmail.com>
2026-05-26 13:45:19 -0700

    Move inset above explicit properties

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,10 +1,19 @@
 import { propertyGroups } from "stylelint-config-clean-order";
 
-const propertiesOrder = propertyGroups.map((properties) => ({
-  noEmptyLineBetween: true,
-  emptyLineBefore: "never",
-  properties
-}));
+const propertiesOrder = propertyGroups.map((properties) => {
+  const insetIdx = properties.indexOf("inset");
+  const topIdx = properties.indexOf("top");
+  if (insetIdx > -1 && topIdx > -1 && insetIdx > topIdx) {
+    const patched = properties.filter((p) => p !== "inset");
+    patched.splice(topIdx, 0, "inset");
+    return {
+      noEmptyLineBetween: true,
+      emptyLineBefore: "never",
+      properties: patched
+    };
+  }
+  return { noEmptyLineBetween: true, emptyLineBefore: "never", properties };
+});
 
 export default {
   extends: ["stylelint-config-clean-order", "stylelint-config-standard-scss"],
