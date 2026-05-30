64fd09eb3c98cbc80fd7b6bd39a7ef7fd09ef2e9
sebnitu <nitu.seb@gmail.com>
2026-05-29 22:04:40 -0700

    Remove usePopover module

--- docs/src/modules/usePopover.js
+++
@@ -1,6 +0,0 @@
-import { teleportElement } from "vrembem";
-
-const popovers = document.querySelectorAll(".popover");
-popovers.forEach((popover) => {
-  teleportElement(popover, ".popovers", "append");
-});
