f5e87c858c4c7bfe5f5ef2aa8ed84d4c5ca81cf4
sebnitu <nitu.seb@gmail.com>
2026-05-31 15:22:58 -0700

    Move smartypants disable to mdx integrations

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx({
+      smartypants: false
+    })
+  ],
   markdown: {
-    smartypants: false,
     shikiConfig: {
       theme: "css-variables"
     }
