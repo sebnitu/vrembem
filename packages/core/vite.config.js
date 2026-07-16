6fa244387fcb86321dd7372989717ba09507d654
sebnitu <nitu.seb@gmail.com>
2026-07-15 21:59:55 -0700

    Remove JS artifacts from core and vrembem packages

--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -1,5 +1,4 @@
-import { resolve } from "path";
 import { createViteConfig } from "../../vite.config.shared.js";
 import data from "./package.json";
 
-export default createViteConfig(data, resolve(import.meta.dirname, "index.ts"));
+export default createViteConfig(data);
