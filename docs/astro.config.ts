f5c33c0f12c6e31fa5871a062fb896ec37f1eca9
sebnitu <nitu.seb@gmail.com>
2026-07-22 21:09:25 -0700

    Switch to esbuild for cssMinify setting

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  vite: {
+    build: {
+      cssMinify: "esbuild"
+    }
+  },
   integrations: [
     mdx({
       smartypants: false
