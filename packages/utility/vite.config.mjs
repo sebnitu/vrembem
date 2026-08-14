9fb0bb65464c5bef1a28849262aee0c670fbe382
Sebastian Nitu <nitu.seb@gmail.com>
2026-08-14 16:58:28 -0700

    Add with type for json imports and use mjs ext for vite configs (#2948)
    This PR fixes two Vite warnings by adding `with { type: "json" }` to json imports and ensuring the vite config files use the `.mjs` extension since packages that don't export JavaScript will not have a type module property.

--- packages/utility/vite.config.mjs
+++ packages/utility/vite.config.mjs
@@ -1,4 +1,4 @@
 import { createViteConfig } from "../../vite.config.shared.js";
-import data from "./package.json";
+import data from "./package.json" with { type: "json" };
 
 export default createViteConfig(data);
