e07bdcea6e06b291f18aed72d6b74317578afa67
sebnitu <nitu.seb@gmail.com>
2026-08-14 16:54:42 -0700

    Add with type for json imports and use mjs ext for vite configs

--- packages/drawer/vite.config.mjs
+++ packages/drawer/vite.config.mjs
@@ -1,5 +1,5 @@
 import { resolve } from "path";
 import { createViteConfig } from "../../vite.config.shared.js";
-import data from "./package.json";
+import data from "./package.json" with { type: "json" };
 
 export default createViteConfig(data, resolve(import.meta.dirname, "index.ts"));
