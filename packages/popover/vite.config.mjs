af2989bf16584417e7017375e661eab1965ae365
Sebastian Nitu <nitu.seb@gmail.com>
2026-08-14 21:06:51 -0700

    Add <vb-popover> custom element (#2949)
    This PR defines the new `<vb-popover>` custom element whos purpose is to add a better UX for popover triggers that have both a popover and a tooltip. This is done by disabling tooltips from displaying while an associated popover is currently active. Tooltip functionality is restored after the popover is closed.
    
    Documentation and tests have also been added for the new popover custom element.
    
    **Additional changes**
    
    - Updated icons for GitHub and packages in Vite demo pages.

--- packages/popover/vite.config.mjs
+++ packages/popover/vite.config.mjs
@@ -1,4 +1,5 @@
+import { resolve } from "path";
 import { createViteConfig } from "../../vite.config.shared.js";
 import data from "./package.json" with { type: "json" };
 
-export default createViteConfig(data);
+export default createViteConfig(data, resolve(import.meta.dirname, "index.ts"));
